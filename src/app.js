4b21d9dc6bdd0bb64cd0f052b738a4d5011c5ddb
Joshua <112779066+Jxshua17@users.noreply.github.com>
2026-05-18 16:30:30 +0100

    fix: added shipments to path.
    also removed everything that had to do with a warehouse.

--- src/app.js
+++ src/app.js
@@ -22,9 +22,10 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Health check
-app.get("/", (req, res) => {
-  res.json({ message: "Multi-Warehouse Supply Chain API is running" });
+app.use("/api/shipments", shipmentRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log(`the current port is ${process.env.PORT}`);
 });
 
 // Routes
