474448066221bd4228e019f00bcb2f7c64250f14
Joshua <112779066+Jxshua17@users.noreply.github.com>
2026-05-19 14:20:40 +0100

    removed certain files from tracking

--- routes/warehouseRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const warehouseController = require("../controller/warehouseController");
-
-router.post("/warehouse", warehouseController.createWarehouse);
-router.get("/warehouses", warehouseController.getWarehouse);
-
-module.exports = router;
